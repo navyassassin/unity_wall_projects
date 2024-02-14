de1224836a6013f0495fb8b1a57af1b675050275
max.breitmeyer <max.breitmeyer@gmail.com>
2024-02-14 16:35:36 -0500

    saved scene

--- test_project/Assets/Scripts/PlayerMovement.cs
+++ test_project/Assets/Scripts/PlayerMovement.cs
@@ -26,7 +26,21 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            rb.velocity = new Vector3(rb.velocity.x, jumpSpeed, rb.velocity.z);
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, jumpSpeed, rb.velocity.z);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy Head"))
+        {
+            Destroy(collision.transform.parent.gameObject);
+            Jump();
         }
     }
 
